1cd930733136b6e6ea86822fd606e0bbddec9d9d
Danas <sutula.danas@gmail.com>
2019-11-01 20:10:06 +0200

    Distance solution is updated at given frequency, e.g. every n'th iteration

--- optim/optim.py
+++ optim/optim.py
@@ -12,6 +12,7 @@ functional to be minimized corresponds to the energy of the deformation.
 
 '''
 
+import sys
 import math
 import dolfin
 import numpy as np
@@ -28,6 +29,8 @@ EPS = 1e-12
 
 
 class TopologyOptimizer:
+
+    MAXIMUM_DISTANCES_UPDATE_PERIOD = 2**(sys.int_info.bits_per_digit-1)
 
     def __init__(self, J, P, C, p, p_locals, equilibrium_solve, equilibrium_write=None):
         '''Minimize the objective functional `J(u(p),p)` (e.g. potential energy
@@ -155,7 +158,7 @@ class TopologyOptimizer:
 
     def optimize(self, stepsize, penalty_weight, collision_distance,
                  convergence_tolerance=None, minimum_convergences=None,
-                 maximum_iterations=None):
+                 maximum_iterations=None, distances_update_frequency=None):
         '''
         Parameters
         ----------
@@ -166,6 +169,11 @@ class TopologyOptimizer:
             of the phasefield penalty gradient `dPdp` and the cost gradient `dJdp`.
         collision_distance : float
             Value for the minimum distance between local phasefields (`p_locals`).
+        distances_update_frequency : float
+            The phasefield distance problem will be solved at approximately every
+            `1 / distances_update_frequency` iterations. The value must be in range
+            [0, 1] where the lower and upper bounds mean that the distance problem
+            is 'solved never' and 'solved at every iteration' respectively.
 
         Returns
         -------
@@ -177,13 +185,13 @@ class TopologyOptimizer:
         '''
 
         if stepsize <= 0.0:
-            raise ValueError('Require `stepsize > 0`')
-
-        if collision_distance < 0.0:
-            raise ValueError('Require `collision_distance > 0`')
+            raise ValueError('Require `stepsize > 0.0`')
 
         if not 0.0 <= penalty_weight <= 1.0:
-            raise ValueError('Require `0 <= penalty_weight <= 1`')
+            raise ValueError('Require `0.0 <= penalty_weight <= 1.0`')
+
+        if collision_distance < 0.0:
+            raise ValueError('Require `collision_distance > 0.0`')
 
         if convergence_tolerance is None: convergence_tolerance = \
             self.parameters_topology_solver['convergence_tolerance']
@@ -194,6 +202,9 @@ class TopologyOptimizer:
         if maximum_iterations is None: maximum_iterations = \
             self.parameters_topology_solver['maximum_iterations']
 
+        if distances_update_frequency is None: distances_update_frequency = \
+            self.parameters_topology_solver['distances_update_frequency']
+
         if convergence_tolerance < 0:
             raise ValueError('Require non-negative `convergence_tolerance`')
 
@@ -203,6 +214,9 @@ class TopologyOptimizer:
         if not (isinstance(maximum_iterations, int) and maximum_iterations > 0):
             raise ValueError('Require positive integer `maximum_iterations`')
 
+        if not 0.0 <= distances_update_frequency <= 1.0:
+            raise ValueError('Require `0.0 <= distances_update_frequency <= 1.0`')
+
         if len(self._p_locals) > 1 and (
            (self._d_arr_locals[:,0] == 0).all() or
            (self._d_arr_locals[:,0] == np.inf).all()):
@@ -211,11 +225,15 @@ class TopologyOptimizer:
         weight_P = penalty_weight
         weight_J = 1.0 - weight_P
 
-        self._collision_distance = collision_distance
-        p_arr = sum(self._p_vec_locals).get_local()
+        distances_update_period = int(1.0/distances_update_frequency) if \
+            distances_update_frequency > 1/self.MAXIMUM_DISTANCES_UPDATE_PERIOD \
+            else self.MAXIMUM_DISTANCES_UPDATE_PERIOD
 
         solve_phasefield_distances = self._solve_phasefield_distances \
             if len(self._p_locals) > 1 else lambda : None # Dummy callable
+
+        self._collision_distance = collision_distance
+        p_arr = sum(self._p_vec_locals).get_local()
 
         self._apply_phasefield_constraints(p_arr)
         self._assign_phasefield_values(p_arr)
@@ -242,7 +260,8 @@ class TopologyOptimizer:
             p_arr_prv = p_arr
             p_arr = p_arr + dp_arr
 
-            solve_phasefield_distances()
+            if not num_iterations % distances_update_period:
+                solve_phasefield_distances()
 
             self._apply_collision_constraints(p_arr)
             self._apply_phasefield_constraints(p_arr)
